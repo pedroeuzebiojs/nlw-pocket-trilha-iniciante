01f5728fe83cb2547e96b0cb03687714e7423290
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-09-13 21:51:13 -0300

    sistema de mensagem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { select, input, checkbox } = require("@inquirer/prompts");
 
+let mensagem = "Bem vindo ao App de Metas";
 let meta = {
   value: "Tomar 3L de água por dia",
   checked: false,
@@ -11,7 +12,7 @@ const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:" });
 
   if (meta.length == 0) {
-    console.log("A meta não pode ser vazia.");
+    mensagem = "A meta não pode ser vazia.";
     return;
   }
 
@@ -19,6 +20,8 @@ const cadastrarMeta = async () => {
     value: meta,
     checked: false,
   });
+
+  mensagem = "Meta cadastrada com sucesso!";
 };
 
 const listarMetas = async () => {
@@ -34,7 +37,7 @@ const listarMetas = async () => {
   });
 
   if (respostas.length == 0) {
-    console.log("Nenhuma meta selecionada!");
+    mensagem = "Nenhuma meta selecionada!";
     return;
   }
 
@@ -46,7 +49,7 @@ const listarMetas = async () => {
     meta.checked = true;
   });
 
-  console.log("Meta(s) marcadas como concluída(s)");
+  mensagem = "Meta(s) marcada(s) como concluída(s)";
 };
 
 const metasRealizadas = async () => {
@@ -55,7 +58,7 @@ const metasRealizadas = async () => {
   });
 
   if (realizadas.length == 0) {
-    console.log("Não existem metas realizadas! :(");
+    mensagem = "Não existem metas realizadas! :(";
     return;
   }
 
@@ -71,7 +74,7 @@ const metasAbertas = async () => {
   });
 
   if (abertas.length == 0) {
-    console.log("Não existem metas abertas! :)");
+    mensagem = "Não existem metas abertas! :)";
     return;
   }
 
@@ -96,7 +99,7 @@ const deletarMetas = async () => {
   });
 
   if (itemsADeletar.length == 0) {
-    console.log("Nenhum item para deletar!");
+    mensagem = "Nenhum item para deletar!";
     return;
   }
 
@@ -106,11 +109,22 @@ const deletarMetas = async () => {
     });
   });
 
-  console.log("Meta(s) deletada(s) com sucesso!");
+  mensagem = "Meta(s) deletada(s) com sucesso!";
+};
+
+const mostrarMensagem = () => {
+  console.clear();
+
+  if (mensagem != "") {
+    console.log(mensagem);
+    console.log("");
+    mensagem = "";
+  }
 };
 
 const start = async () => {
   while (true) {
+    mostrarMensagem();
     const opcao = await select({
       message: "Menu >",
       choices: [
--- todo.md
+++ todo.md
@@ -4,4 +4,4 @@
   - [x] Metas abertas
 - [x] Marcar/Desmarcar metas como concluída
 - [x] Remover metas
-- Sistema de mensagem
+- [x] Sistema de mensagem
